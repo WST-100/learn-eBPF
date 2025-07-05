fcdb7bd67ecb31456194acac86c4c1f1a103b07f
WST-100 <1744156571@qq.com>
2025-07-05 16:40:48 +0800

    PROG

--- fentry-link.bpf.c
+++ fentry-link.bpf.c
@@ -1,7 +1,7 @@
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
-#include <bpf/bpf_core_read.h>
+// #include <bpf/bpf_core_read.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
 
@@ -9,24 +9,24 @@ SEC("fentry/do_unlinkat")
 int BPF_PROG(do_unlinkat, int dfd, struct filename* name)
 {
     pid_t pid;
-    const char* filename;
+    // const char* filename;
 
     pid = bpf_get_current_pid_tgid() >> 32;
-    filename = BPF_CORE_READ(name, name);
-    bpf_printk("fentry: pid = %d, filename = %s\n", pid, filename);
+    // filename = BPF_CORE_READ(name, name);
+    bpf_printk("fentry: pid = %d, filename = %s\n", pid, name->name);
 
     return 0;
 }
 
 SEC("fexit/do_unlinkat")
-int BPF_FROG(do_unlinkat_exit, int dfd, struct filename* name, long ret)
+int BPF_PROG(do_unlinkat_exit, int dfd, struct filename* name, long ret)
 {
     pid_t pid;
-    const char* filename;
+    // const char* filename;
 
     pid = bpf_get_current_pid_tgid() >> 32;
-    filename = BPF_CORE_READ(name, name);
-    bpf_printk("fexit: pid = %d, filename = %s, ret = %ld\n", pid, filename, ret);
+    // filename = BPF_CORE_READ(name, name);
+    bpf_printk("fexit: pid = %d, filename = %s, ret = %ld\n", pid, name->name, ret);
 
     return 0;
 }
