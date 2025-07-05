d7fa289bc185446469aad44af7cc98a8f1a43204
WST-100 <1744156571@qq.com>
2025-07-05 16:12:32 +0800

    lesson3 error

---
+++ fentry-link.bpf.c
@@ -0,0 +1,32 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <bpf/bpf_core_read.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+SEC("fentry/do_unlinkat")
+int BPF_PROG(do_unlinkat, int dfd, struct filename* name)
+{
+    pid_t pid;
+    const char* filename;
+
+    pid = bpf_get_current_pid_tgid() >> 32;
+    filename = BPF_CORE_READ(name, name);
+    bpf_printk("fentry: pid = %d, filename = %s\n", pid, filename);
+
+    return 0;
+}
+
+SEC("fexit/do_unlinkat")
+int BPF_FROG(do_unlinkat_exit, int dfd, struct filename* name, long ret)
+{
+    pid_t pid;
+    const char* filename;
+
+    pid = bpf_get_current_pid_tgid() >> 32;
+    filename = BPF_CORE_READ(name, name);
+    bpf_printk("fexit: pid = %d, filename = %s, ret = %ld\n", pid, filename, ret);
+
+    return 0;
+}
